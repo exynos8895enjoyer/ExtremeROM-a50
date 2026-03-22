b06902133b1c4ba5eda3e1d67c1acd698e4aabfb
exynos8895enjoyer <kostadinpiponkov@gmail.com>
2026-03-22 22:22:41 +0200

    add a50
    bomb

---
+++ target/a50/config.sh
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy A50 (Exynos) (a50)
+TARGET_NAME="Galaxy A50 (Exynos)"
+TARGET_CODENAME="a50"
+TARGET_ASSERT_MODEL=("SM-A505FN")
+TARGET_PLATFORM="exynos5"
+TARGET_FIRMWARE="SM-A505FN/BGL/356642105439862"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=30
+TARGET_PRODUCT_FIRST_API_LEVEL=28
+TARGET_VNDK_VERSION=30
+TARGET_SINGLE_SYSTEM_IMAGE="essi"
+TARGET_OS_FILE_SYSTEM="erofs"
+TARGET_SUPER_PARTITION_SIZE=0
+TARGET_SUPER_GROUP_NAME="group_basic"
+TARGET_SUPER_GROUP_SIZE=0
+TARGET_HAS_SYSTEM_EXT=false
+
+# SEC Product Feature
+TARGET_AUTO_BRIGHTNESS_TYPE="1"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_universal9610"
+TARGET_NFC_CHIP_VENDOR="SLSI"
+TARGET_FP_SENSOR_CONFIG="google_touch_display"
+TARGET_HAS_MASS_CAMERA_APP=false
+TARGET_HAS_QHD_DISPLAY=false
+TARGET_HFR_MODE="0"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="60"
+TARGET_HFR_DEFAULT_REFRESH_RATE="60"
+TARGET_DISPLAY_CUTOUT_TYPE="center"
+TARGET_HFR_SEAMLESS_BRT="none"
+TARGET_HFR_SEAMLESS_LUX="none"
+TARGET_IS_ESIM_SUPPORTED=false
+TARGET_HAS_HW_MDNIE=true
+TARGET_MDNIE_SUPPORTED_MODES="130"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="3"
+TARGET_SUPPORT_WIFI_7=false
+TARGET_SUPPORT_HOTSPOT_DUALAP=false
+TARGET_SUPPORT_HOTSPOT_WPA3=false
+TARGET_SUPPORT_HOTSPOT_6GHZ=false
+TARGET_SUPPORT_HOTSPOT_WIFI_6=false
+TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN=false
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
